dc0cbf414215fd4bc516258012f4ed27dce8bbe9
YulyaVlasenko <correspondenceoff@gmail.com>
2023-08-24 18:15:24 +0300

    ref

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,23 +1,20 @@
-import { useSelector, useDispatch } from 'react-redux';
-import * as s from './Filter.styled';
-import { handleCangeFilter } from 'redux/filterSlise';
 
-export const Filter = () => {
-  const dispatch = useDispatch();
-  const filter = useSelector(state => state.filterUser.filter);
+import PropTypes from 'prop-types';
+import React from 'react'
 
+const Filter = ({title, value, handleChangeFilter}) => {
   return (
-    <>
-      <s.Description>Find contacts by name</s.Description>
-      <s.Input
-        type="text"
-        name="name"
-        pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-        title="Name may contain only letters, apostrophe, dash and spaces. 
-          For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-        value={filter}
-        onChange={e => dispatch(handleCangeFilter(e.target.value.trim()))}
-      />
-    </>
-  );
-};
+      <form>
+          <label htmlFor="#">{title}: </label>
+          <input type="text" name="filter" onChange={handleChangeFilter} value={value} />
+      </form>
+  )
+}
+
+export default Filter
+
+Filter.propTypes = {
+    title: PropTypes.string.isRequired,
+    value: PropTypes.string.isRequired,
+    handleChangeFilter: PropTypes.func.isRequired,
+}
